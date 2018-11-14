4487f596b9c00aef39da09b3fdeb53b4ccbe4027
sebastien <sebastien.degand@etu.unice.fr>
2018-11-14 20:53:51 +0100

    UploadServlet: use jsonobject

--- project/UploadHandler/src/main/java/fr/polytech/UploaderServlet.java
+++ project/UploadHandler/src/main/java/fr/polytech/UploaderServlet.java
@@ -45,6 +45,7 @@ import org.joda.time.DateTime;
 import org.joda.time.DateTimeZone;
 import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
+import org.json.JSONObject;
 
 // [START example]
 @SuppressWarnings("serial")
@@ -130,7 +131,8 @@ public class UploaderServlet extends HttpServlet {
     }
     in.close();
 
-    return response.toString();
+    JSONObject jsonResponse = new JSONObject(response.toString());
+    return jsonResponse.getString("fileId");
   }
 }
 // [END example]
