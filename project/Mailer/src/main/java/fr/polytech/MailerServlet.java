f6251cf7fc159fa6f625c032f864615b7e975b01
Shiyang HUANG <shiyang.huang@etu.unice.fr>
2018-11-14 20:12:40 +0100

    first implement mailer

---
+++ project/Mailer/src/main/java/fr/polytech/MailerServlet.java
@@ -0,0 +1,57 @@
+package fr.polytech;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.Properties;
+
+@WebServlet(name = "MailerServlet")
+public class MailerServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String address = request.getParameter("address");
+        String subject = request.getParameter("subject");
+        String content = request.getParameter("content");
+        if (address != null && content != null) {
+            try {
+                sendSimpleMail(address, subject, content);
+            } catch (AddressException e) {
+                response.setStatus(400);
+                response.getWriter().print("Wrong address.");
+                return;
+            } catch (MessagingException e) {
+                response.setStatus(400);
+                response.getWriter().print("Messaging problem.");
+                return;
+            } catch (UnsupportedEncodingException e) {
+                response.setStatus(400);
+                response.getWriter().print("Unsupported encoding.");
+                return;
+            }
+            response.getWriter().print("Sending email to " + address);
+        } else {
+            response.getWriter().print("Need address/content.");
+        }
+    }
+
+    private void sendSimpleMail(String address, String subject, String content) throws AddressException, MessagingException, UnsupportedEncodingException {
+        Properties props = new Properties();
+        Session session = Session.getDefaultInstance(props, null);
+        Message msg = new MimeMessage(session);
+        msg.setFrom(new InternetAddress("no-reply@[App_Name].appspotmail.com", "polyshare"));
+        msg.addRecipient(Message.RecipientType.TO, new InternetAddress(address, "User"));
+        msg.setSubject(subject);
+        msg.setText(content);
+        Transport.send(msg);
+    }
+}
